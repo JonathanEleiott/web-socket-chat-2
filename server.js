2ed750711a7990278e54a4687e2ae6e3d4e92d8a
Jonathan Eleiott <jonathan.eleiott@fullstackacademy.com>
2024-12-04 10:43:45 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+  console.log(`CONNECTION MADE`);
+
+  socket.on('new message sent', (newMessage) => {
+    console.log(`A NEW MESSAGE HAS BEEN SENT OUT`);
+    console.log(newMessage);
+
+    socket.emit('new message to relay', newMessage);
+  });
+});
+
+app.get('/', (req, res, next) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`listening on port ${PORT}`));
